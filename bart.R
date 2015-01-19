a6c35e1c4531b26eaed8f31371da9dc6ac58429f
Matt Taddy <taddy@chicagobooth.edu>
2015-01-19 16:34:46 -0600

    bart file

--- bart.R
+++ bart.R
@@ -1,32 +1,5 @@
 library(BayesTree)
 
-##simulate data (example from Friedman MARS paper)
-fried = function(x){
-10*sin(pi*x[,1]*x[,2]) + 20*(x[,3]-.5)^2+10*x[,4]+5*x[,5]
-}
-
-n = 100     
-p = 10
-B = 100
-
-rmse = c()
-
-for(b in 1:B){
-	print(b)
-	xtrain = matrix(runif(n*p),n,p) 
-	xtest = matrix(runif(1000*p),1000,p) 
-	y = fried(xtrain) + rnorm(n)
-	f = fried(xtest)
-	bartFit = bart(xtrain,y,xtest,ntree=100)
-	fhat = bartFit$yhat.test.mean
-	rmse <- c(rmse, sqrt(mean( (fhat-f)^2 )))
-}
-mean(rmse)
-# 1.7
-
-###########################
-library(BayesTree)
-
 cah = read.csv("data/CAhousing.csv")
 y = cah["medianHouseValue"][,1]
 X = cah[,1:8]
@@ -46,4 +19,30 @@ for(k in 0:9){
 }
 
 write.table(hrmse, "graphs/bartca.txt", row.names=FALSE, col.names=FALSE)
+
+###########################
+
+# ##simulate data (example from Friedman MARS paper)
+# fried = function(x){
+# 10*sin(pi*x[,1]*x[,2]) + 20*(x[,3]-.5)^2+10*x[,4]+5*x[,5]
+# }
+
+# n = 100     
+# p = 10
+# B = 100
+ 
+# rmse = c()
+
+# for(b in 1:B){
+# 	print(b)
+# 	xtrain = matrix(runif(n*p),n,p) 
+# 	xtest = matrix(runif(1000*p),1000,p) 
+# 	y = fried(xtrain) + rnorm(n)
+# 	f = fried(xtest)
+# 	bartFit = bart(xtrain,y,xtest,ntree=100)
+# 	fhat = bartFit$yhat.test.mean
+# 	rmse <- c(rmse, sqrt(mean( (fhat-f)^2 )))
+# }
+# mean(rmse)
+# # 1.7
 
